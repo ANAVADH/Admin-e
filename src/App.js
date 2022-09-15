28935223825da9b737b0a280a6ebdaf1c59e5d1c
ANAVADH <anavadhsuresh1042@gmail.com>
2022-09-15 13:49:57 +0530

    10

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from "./components/sidebar/Sidebar";
+import Sidebar from "./components/sidebar/Sidebar.jsx";
 import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Home from "./pages/home/Home";
