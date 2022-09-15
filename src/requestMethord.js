ccb6df9dec23ea96f1b6d2f7e3de118eb865bb31
ANAVADH <anavadhsuresh1042@gmail.com>
2022-09-15 13:41:14 +0530

    10

--- src/requestMethord.js
+++ src/requestMethord.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const Url = location.hostname === "localhost"? "http://localhost:8080":"https://shopify-rest-server.herokuapp.com";
+const Url = "https://shopify-rest-server.herokuapp.com";
 const BASE_URL = `${Url}/api`;
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)?.currentUser?.token;
 
